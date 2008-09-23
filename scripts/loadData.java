87d6d01ba76c21e72e29286c5d5cb936013f762b
wes@tetriscodes.com <wes@tetriscodes.com@f6881ea2-86b9-11dd-a7e4-01043650f904>
2008-09-23 04:18:34 +0000

    adding loadData.java

---
+++ scripts/loadData.java
@@ -0,0 +1,5 @@
+import java.lang.*;
+
+public class loadData {
+
+}
---
+++ scripts/write.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.io.*;
+
+public class write {
+
+	public static void main(String[] args) {
+		Writer output = null;
+		File file = new File("create.sql");
+		try {
+		output = new BufferedWriter(new FileWriter(file));
+		output.write("CREATE TABLE rawData (\n");
+		output.write("y VARCHAR( 50 ) NULL,\n");	
+		for(int i=0; i < 63; i++) {
+			output.write("x" + i + " VARCHAR( 50 ) NULL,\n");
+		}
+		output.write("x VARCHAR( 50 ) NULL\n");
+		output.write(");");
+		output.close();
+		} catch (Exception e) { }
+	}
+
+}
