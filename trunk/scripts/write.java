6ece5a5ed7ccdecb141f375ba8f8266d0f46125f
wes@tetriscodes.com <wes@tetriscodes.com@f6881ea2-86b9-11dd-a7e4-01043650f904>
2008-09-24 00:53:21 +0000

    corrected table column names

--- trunk/scripts/write.java
+++ trunk/scripts/write.java
@@ -10,10 +10,10 @@ public class write {
 		output = new BufferedWriter(new FileWriter(file));
 		output.write("CREATE TABLE rawData (\n");
 		output.write("y VARCHAR( 50 ) NULL,\n");	
-		for(int i=0; i < 63; i++) {
+		for(int i=1; i < 63; i++) {
 			output.write("x" + i + " VARCHAR( 50 ) NULL,\n");
 		}
-		output.write("x VARCHAR( 50 ) NULL\n");
+		output.write("id VARCHAR( 50 ) NULL\n");
 		output.write(");");
 		output.close();
 		} catch (Exception e) { }
