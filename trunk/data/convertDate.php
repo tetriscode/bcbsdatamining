904f59d24b09a1f3698f6340a9a44a38cd3c7403
wes@tetriscodes.com <wes@tetriscodes.com@f6881ea2-86b9-11dd-a7e4-01043650f904>
2008-10-15 04:15:50 +0000

    deleting split

--- trunk/data/convertDate.php
+++ trunk/data/convertDate.php
@@ -5,10 +5,26 @@ $fo = fopen("trainingMod.csv","w");
 while(($data = fgetcsv($fh,2000,",")) !== FALSE) {
 	$num = count($data);
 	for($c=0; $c < $num-1; $c++) {
-		fwrite($fo,"$data[$c]");
-		fwrite($fo,",");		
+		if($c == 6 || $c == 10 || $c == 11) {
+			if(strlen($data[$c]) > 0){
+				$dte = convertDate($data[$c]);
+				fwrite($fo,$dte);
+			}
+			fwrite($fo,",");
+		}else {
+			fwrite($fo,"$data[$c]");
+			fwrite($fo,",");
+		}		
 	}
-	fwrite($fo,$data[$num-1]);	
+	fwrite($fo,$data[$num-1]."\n");	
+}
+
+function convertDate($str) {
+	$dte = "";
+	$dte = $dte . substr($str,6,4);
+	$dte = $dte . "-" . substr($str,0,2);
+	$dte = $dte . "-" . substr($str,3,2);
+	return $dte;
 }
 
 fclose($fh);
